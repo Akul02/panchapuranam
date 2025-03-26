2e63142438ba3546dc8889af2ab0602dbbb7e522
Akul <akul.anand52@gmail.com>
2025-03-26 20:28:23 +1100

    basic layout  of home page

---
+++ frontend/src/app/components/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home () {
+    return (
+        <div className="home">
+            <div className="title"><h1>Panchapuranam</h1></div>
+            <div className="instructions"><p>If you have memorised and sung the below 5  songs, we will honor you by issuing a certificate. Contact us when you are ready. No age limit</p></div>
+            <div className="songs">songs</div>
+            <div className="footer">footer</div>
+        </div>
+    );
+}
---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Navbar () {
+    return (
+        <nav>
+            <h1>Language</h1>
+            <h1>Events</h1>
+            <h1>Teachers Login</h1>
+            {/*<Link href="/events">Events</Link>*/}
+        </nav>
+    );
+}
