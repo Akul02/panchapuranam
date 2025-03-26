c9bf2ab781033365576ba4b8c7a83adc26117201
Akul <akul.anand52@gmail.com>
2025-03-26 22:03:04 +1100

    basic structure of page completed

--- frontend/src/app/components/Home.tsx
+++ frontend/src/app/components/Home.tsx
@@ -1,9 +1,11 @@
+import Songs from "./Songs";
+
 export default function Home () {
     return (
         <div className="home">
             <div className="title"><h1>Panchapuranam</h1></div>
-            <div className="instructions"><p>If you have memorised and sung the below 5  songs, we will honor you by issuing a certificate. Contact us when you are ready. No age limit</p></div>
-            <div className="songs">songs</div>
+            <div className="instructions"><p>If you have memorised and sung the below 5  songs, we will honor you by issuing a certificate. Contact us when you are ready. No age limit.</p></div>
+            <Songs/>
             <div className="footer">footer</div>
         </div>
     );
