7fb5493e9201d8369829abf959e512601fd68400
Akul <akul.anand52@gmail.com>
2025-06-19 19:18:05 +1000

    fixed build errors (linting errors)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 
 //  components
-import Navbar from "../components/Navbar";
 import LanguageProvider from "../context/LanguageContext";
 
 export const metadata: Metadata = {
