fcdc434f1358217fd6f04c072df2d92bfe72b4cc
Akul <akul.anand52@gmail.com>
2026-02-09 18:58:37 +1100

    adding optional chaining to avoid mapping over undefined

--- frontend/components/features/songs/Songs.tsx
+++ frontend/components/features/songs/Songs.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import useLanguage from '../../../hooks/useLanguage';
 import { Song } from '../../../types/song';
+
 import Audio from './Audio';
 
 export default function Songs() {
@@ -23,7 +24,7 @@ export default function Songs() {
 
     return (
         <div className="flex flex-col items-center gap-y-8 mt-8 mb-20 w-full">
-            {songs.map((song) => (
+            {songs.length > 0 && songs.map((song) => (
                 <div className="w-1/2 rounded-xl p-4 text-primary shadow-[0_4px_8px_0_rgba(0,0,0,0.2),_0_6px_20px_0_rgba(0,0,0,0.19)]" key={song.songId}>
                     <p className='text-3xl font-bold'>{song.title} </p>
                     {song.verse.map((line, index) => (
